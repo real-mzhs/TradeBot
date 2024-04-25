cc10759edf11c4bbe2e96380bbf3f4027ec9a9b4
Niftaliev Rustam <rustam.niftaliev97@gmail.com>
2024-04-25 18:19:36 +0400

    Bug Fix

--- Desktop/Desktop/Services/Classes/RegistrationService.cs
+++ Desktop/Desktop/Services/Classes/RegistrationService.cs
@@ -2,6 +2,7 @@
 using Desktop.Services.Interfaces;
 using Desktop.Services.Network.API;
 using Desktop.Services.Network.Responses;
+using RestSharp;
 
 namespace Desktop.Services.Classes;
 
@@ -16,7 +17,9 @@ public class RegistrationService : IRegistrationService
 
     public async Task<DataResponse<RegistrationResponse>> RegistrationAsync(User user)
     {
-        return await _tradeClient.Post<RegistrationResponse>("/user/register", user); 
+        var parameter = Parameter.CreateParameter("UserId", user.Id.ToString(), ParameterType.UrlSegment);
+        var parameters = new Parameter[] { parameter };
+        return await _tradeClient.Post<RegistrationResponse>("/user/register", parameters); 
     }
 }
 
